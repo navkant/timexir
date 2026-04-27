156e80aace457cad3761bd12735c59093cc7718d
Navkant Tyagi <navkant.tyagi9@gmail.com>
2026-04-27 19:22:38 +0530

    added suspense api for slow apis

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -51,6 +51,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
